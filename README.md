749dd3e6431f985bbbb2e868e39745a507501eec
Stephen Ceresia <stephen@stephenceresia.ca>
2020-03-08 17:57:40 -0400

    refactoring

---
+++ README.md
@@ -0,0 +1,4 @@
+# TermsAndConditions
+SwiftUI + Combine: Terms and Conditions example
+
+<img src = "termsandconditions.gif" width="480">
--- TermsAndConditions/TermsAndConditionsView.swift
+++ TermsAndConditions/TermsAndConditionsView.swift
@@ -11,28 +11,25 @@ import Combine
 
 struct TermsAndConditionsView: View {
     
-    @ObservedObject private var model = TermsAndConditionsViewModel()
-    
-    @State private var showingSheet = false
-    
-    struct Checkmark : View {
-         @Binding var isValidAge: Bool
+    var body: some View {
         
-        var body: some View {
-            Image(systemName: isValidAge ? "checkmark.circle" : "xmark.circle").foregroundColor(isValidAge ? .green : .red)
-            .font(.system(size: 32, weight: .medium))
+        NavigationView {
+            TermsAndConditionsForm()
         }
     }
     
+}
+
+struct TermsAndConditionsForm : View {
+    
+    @ObservedObject private var model = TermsAndConditionsViewModel()
+        
     var body: some View {
         
         VStack {
-            List {
-                Spacer()
-                
-                Section(header:Text("Terms and Conditions").font(Font.system(.title).bold())) {
-
-                    VStack {
+            Form {
+                Section {
+                    Section {
                         HStack {
                             TextField("Enter your age (18+ only)", text: $model.ageEntered)
                                 .textFieldStyle(RoundedBorderTextFieldStyle())
@@ -42,46 +39,59 @@ struct TermsAndConditionsView: View {
                             }
                         }
                     }
-                    
-                    HStack {
+                    Section {
                         Toggle(isOn: $model.readAgreement) {
                             Text("Read Terms & Conditions")
                         }
-                    }
-                    HStack {
                         Toggle(isOn: $model.agreeTerms) {
                             Text("Agree to Terms & Conditions")
                         }
-                    }
-                    HStack {
                         Toggle(isOn: $model.acceptMarketingEmails) {
                             Text("Accept to receive email")
                         }
                     }
-                }
-                .padding()
-            }
+                    
+                }.padding()
+                
+            }.navigationBarTitle("Terms and Conditions")
             
             ZStack {
-                Button(action: {
-                    self.showingSheet = true
-                }) {
-                    Text("Continue")
-                }
-                .actionSheet(isPresented: $showingSheet) {
-                    ActionSheet(title: Text("Good to go 👌"), message: nil, buttons: [.cancel(Text("Dismiss"))])
-                }
-                .foregroundColor(.white)
-                .padding()
-                .background(Color.accentColor)
-                .cornerRadius(8)
-                .disabled(!model.isEnabled)
+                ContinueButton().disabled(!model.isEnabled)
             }
         }
         
     }
 }
 
+struct ContinueButton : View {
+    @State private var showingSheet = false
+
+    var body: some View {
+        Button(action: {
+            self.showingSheet = true
+        }) {
+            Text("Continue")
+        }
+        .actionSheet(isPresented: $showingSheet) {
+            ActionSheet(title: Text("Good to go 👌"), message: nil, buttons: [.cancel(Text("Dismiss"))])
+        }
+        .foregroundColor(.white)
+        .padding()
+        .background(Color.accentColor)
+        .cornerRadius(8)
+    }
+}
+
+struct Checkmark : View {
+    @Binding var isValidAge: Bool
+    
+    var body: some View {
+        Image(systemName: isValidAge ? "checkmark.circle" : "xmark.circle").foregroundColor(isValidAge ? .green : .red)
+            .font(.system(size: 32, weight: .medium))
+    }
+}
+
+// PREVIEW
 struct TermsAndConditionsView_Previews: PreviewProvider {
     static var previews: some View {
         TermsAndConditionsView()
@@ -90,3 +100,4 @@ struct TermsAndConditionsView_Previews: PreviewProvider {
             .environment(\.colorScheme, .dark)
     }
 }
+// PREVIEW
